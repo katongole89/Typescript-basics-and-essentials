fa7d12b1ee9a3b785d92eb9cdfa1814439c24c27
katongole89 <katongolerichard089@gmail.com>
2022-11-23 12:31:22 +0300

    added function optionl and default parameters

---
+++ 13FnOptionalAndDefaultParams.ts
@@ -0,0 +1,20 @@
+export {}
+
+export {}
+
+const add = (n1:number, n2?:number):number => {
+    if(n2){
+        return n1+n2
+    }else{
+        return n1
+    }
+    
+}
+
+const add2 = (n1:number, n2:number = 20):number => {
+    return n1+n2
+}
+
+// console.log(add('nameless j', 'df'))
+
+// try to put the mandatory parameters first and then the optional ones
