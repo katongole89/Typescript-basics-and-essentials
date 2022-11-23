3fab37884fb38fd8dd1fd4c1f8ebac96dfa1ec5f
katongole89 <katongolerichard089@gmail.com>
2022-11-23 11:12:34 +0300

    added the any and unknown datatypes

---
+++ 9DatatypesAnyAndUnknown.ts
@@ -0,0 +1,14 @@
+export {}
+
+//enables allowing of any datatype
+let name:any = 'name';
+name = 20
+
+let school:unknown = 'namilyango'
+school = 20
+
+console.log(name.toUpperCase())
+console.log(school.toUpperCase())
+
+
+
