a6478864787864b4f6680d47de0594fd8a180692
katongole89 <katongolerichard089@gmail.com>
2022-11-23 12:37:18 +0300

    added string concatenation

--- 13FnOptionalAndDefaultParams.ts
+++ 13FnOptionalAndDefaultParams.ts
@@ -1,14 +1,11 @@
 export {}
 
-export {}
-
 const add = (n1:number, n2?:number):number => {
     if(n2){
         return n1+n2
     }else{
         return n1
     }
-    
 }
 
 const add2 = (n1:number, n2:number = 20):number => {
