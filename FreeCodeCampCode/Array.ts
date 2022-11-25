a38d82061afde4f0dd987bad017ea908a6fdd3b5
katongole89 <katongolerichard089@gmail.com>
2022-11-25 15:22:10 +0300

    added types to arrays

--- FreeCodeCampCode/Array.ts
+++ FreeCodeCampCode/Array.ts
@@ -1,6 +1,15 @@
 export {}
 
-// let superHeros: string[] = []
-let superHeros: Array<string> = []
+let superHeros: string[] = []
+// let superHeros: Array<string> = []
 superHeros.push('try')
 console.log(superHeros)
+
+type User = {
+    name:string;
+    age:number
+}
+
+let justArr: User[] = []
+
+justArr.push({name:'kat', age:20})
