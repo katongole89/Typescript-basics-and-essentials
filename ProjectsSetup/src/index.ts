9610586cd7bc812ab3a3b8c0f5353dc10e56ded9
katongole89 <katongolerichard089@gmail.com>
2022-11-28 15:41:35 +0300

    added class inheritence

--- ProjectsSetup/src/index.ts
+++ ProjectsSetup/src/index.ts
@@ -20,14 +20,55 @@
 
 // Sr dev code
 class User{
+    private _courseCount = 1
+
     readonly region:string ='central'
     constructor(
         public email:string,
         public name:string
         ){        
     }
+
+    //getters and setters
+    get getAppleEmail():string{
+        return `apple${this.email}`
+    }
+
+    get courseCount():number{
+        return this._courseCount
+    }
+
+    set courseCount(courseNum:number){
+        this._courseCount = courseNum
+    }
+    //setter doesnt return anything - not even void not allowed
 }
 
 //creating an object from the class
 const testObj = new User('ka@k.com', 'rick')
 console.log(testObj.name)
+
+
+//inheritence
+class Admin extends User{
+    isFamily:boolean
+
+    //u can do without this if no variables above or defaults set 
+    constructor(email:string, name:string, isFamily:boolean){
+        super(email, name)
+        this.isFamily = isFamily
+    }
+
+    changeCou(){
+        this.courseCount +=4
+    }
+}
+
+// eg no variables above or their defaults are set
+// class Admin extends User{
+//     changeCou(){
+//         this.courseCount +=4
+//     }
+// }
+
+const newAdmin = new Admin('kat@g.com', 'kat', false)
