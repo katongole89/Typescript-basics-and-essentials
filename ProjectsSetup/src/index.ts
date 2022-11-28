d3ff96ab9c8eeec19e95ed1ee3b0e597730d70d9
katongole89 <katongolerichard089@gmail.com>
2022-11-28 14:58:55 +0300

    added classes-private,public,protected and readonly

--- ProjectsSetup/src/index.ts
+++ ProjectsSetup/src/index.ts
@@ -1,1 +1,33 @@
-console.log('sumn is here')
+// private only here - only accessible in class
+// protected - here and children
+// public -accessed everywhere --default
+
+
+// Jr dev code
+//Classes
+// class User{
+//     // name:string
+//     email:string
+//     name:string
+//     city:string = 'df' //if not passed in a constructor then set default
+//     readonly region:string ='central' //this value can not be changed
+
+//     constructor(email:string, name:string){
+//         this.email = email
+//         this.name = name
+//     }
+// }
+
+// Sr dev code
+class User{
+    readonly region:string ='central'
+    constructor(
+        public email:string,
+        public name:string
+        ){        
+    }
+}
+
+//creating an object from the class
+const testObj = new User('ka@k.com', 'rick')
+console.log(testObj.name)
