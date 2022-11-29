93afb8e804518b82ff4e4b5f53743e6414244871
katongole89 <katongolerichard089@gmail.com>
2022-11-29 11:48:33 +0300

    added extends to generics

--- ProjectsSetup/src/generics.ts
+++ ProjectsSetup/src/generics.ts
@@ -24,3 +24,35 @@ interface Bottle{
 // identityFive<Bottle>({})
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+// ++++++++++++++++++++++ Arrays and interfaces +++++++++++++++++++++++++++++++++++
+const getSearchProduct =<T>(products:T[]):T =>{
+    return products[3]
+}
+//added qomma to separate jsx from generics
+const getSearchProductty =<T,>(products:T[]):T =>{
+    return products[3]
+}
+
+//explanation- take T -- array of T return T ----meaning return value type in the array-- 
+//Research what if the array has two types
+
+// let testArr12: (string|number)[] = ['wer', 2, 3, 'wr']
+// getSearchProduct(testArr12)
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// +++++++++++++++++++++++++ extends +++++++++++++++++++++++++++++++++++++++++++++ 
+function anotherFunc <T, U extends number>(val1:T, val2:U ):object{
+    return {val1, val2}
+}
+// anotherFunc(2, '4') //wont allow coz U must be any form of number
+interface Database{
+    connection:string,
+    username: string,
+    password:string
+}
+function anotherFunc2 <T, U extends Database>(val1:T, val2:U ):object{
+    return {val1, val2}
+}
+
+
