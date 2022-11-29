4e2fa59f602ec4ad03532a0fc8ead575b4cd153d
katongole89 <katongolerichard089@gmail.com>
2022-11-29 12:33:57 +0300

    added the in key word for detections

--- ProjectsSetup/src/detection.ts
+++ ProjectsSetup/src/detection.ts
@@ -1,4 +1,7 @@
-// DETECTION PROBLEMS
+// DETECTION PROBLEMS -- TYPE NARROWING
+// --typeof
+// --using !
+// -- in
 
 const detectType = (val:number|string):void =>{
     if(typeof val === 'number'){
@@ -12,5 +15,24 @@ const detectType2 = (val:string|null):void =>{
     if(!val){
         console.log('please provide val')
     }
+}
 
+//narrowing down interfaces and types
+interface User{
+    name:string,
+    email:string
+}
+
+interface Admin{
+    name:string,
+    email:string,
+    isAdmin:boolean
+}
+
+function isAdminCheck(account:User|Admin):boolean{
+    if('isAdmin' in account){
+        return account.isAdmin
+    }else{
+        return false
+    }
 }
